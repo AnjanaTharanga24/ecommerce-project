449f23a983e4a83bcd4b3a25001eddb42d16da1c
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-02-10 11:11:19 +0530

    create logic for get product by id

--- backend/src/main/java/com/example/backend/service/ProductService.java
+++ backend/src/main/java/com/example/backend/service/ProductService.java
@@ -13,6 +13,7 @@ public interface ProductService {
 
     ProductResponse addProduct(ProductRequest productRequest);
     List<ProductResponse> getAllProducts();
+    Product getProductById(Long id);
     Page<Product> findProductByCategory(Long id, Pageable pageable);
     List<ProductCategory> getAllProductCategories();
     Page<Product> productsSearchByName(String name , Pageable pageable);
